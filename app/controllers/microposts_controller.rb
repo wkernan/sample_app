1eb1ee56b154bbab8ab9e67ac979c763af882deb
Bill Kernan <wkernan@gmail.com>
2013-07-26 14:11:45 -0500

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
 	before_filter :signed_in_user, only: [:create, :destroy]
-	before_filter :current_user,   only: :destroy
+	before_filter :correct_user,   only: :destroy
 
 	def create
 		@micropost = current_user.microposts.build(params[:micropost])
@@ -15,17 +15,14 @@ class MicropostsController < ApplicationController
 
 	def destroy
 		@micropost.destroy
+		flash[:warning] = "Micropost deleted!"
 		redirect_to root_url
 	end
 
 	private
 
-	def micropost_params
-		params.require(:micropost).permit(:content)
-	end
-
-	def correct_user
-		@micropost = current_user.microposts.find_by(id: params[:id])
-		redirect_to root_url if @micropost.nil?
-	end
+		def correct_user
+			@micropost = current_user.microposts.find_by_id(params[:id])
+			redirect_to root_url if @micropost.nil?
+		end
 end
